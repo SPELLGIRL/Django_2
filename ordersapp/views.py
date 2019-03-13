c2a2f47a644e1f939a74be2118c5a5fc265c7459
SPELLGIRL <chepushilka@yandex.ru>
2019-03-13 10:37:53 +0300

    Фиксы контроллеров для обеспечения правильной работы миграций.

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -30,9 +30,9 @@ def get_product_price(request, pk):
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(sender, update_fields, instance, **kwargs):
     if update_fields is 'quantity' or 'product':
-        if instance.pk:
-            instance.product.quantity -= instance.quantity - \
-                                         sender.get_item(instance.pk).quantity
+        if instance.pk and sender.get_item(instance.pk) is not None:
+            instance.product.quantity -= instance.quantity - sender.get_item(
+                instance.pk).quantity
         else:
             instance.product.quantity -= instance.quantity
         instance.product.save()
