0fb5d0c51dd7ff7f8f1f7d7b76300f278cd79ea1
SPELLGIRL <chepushilka@yandex.ru>
2019-03-16 19:36:37 +0300

    Добавлены приложения для отладки. Сделан отдельный файл требований для разработки.

--- geekmarket/settings/development.py
+++ geekmarket/settings/development.py
@@ -60,6 +60,8 @@ INSTALLED_APPS = [
     'social_django',
     'debug_toolbar',
     'template_profiler_panel',
+    'django_extensions',
+    'pydotplus',
     'django_cleanup.apps.CleanupConfig',
 ]
 
@@ -219,6 +221,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
