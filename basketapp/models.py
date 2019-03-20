7ed8f1e137da6c9c4459cfe307870bd02b518bb4
SPELLGIRL <chepushilka@yandex.ru>
2019-03-20 15:01:49 +0300

    1. К методам модели корзины применён декоратор «@cached_property».

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils.functional import cached_property
+
 from mainapp.models import Product
 
 
@@ -27,8 +29,35 @@ class Basket(models.Model):
         return Basket.objects.filter(user=user).order_by(
             'product__category')
 
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related().order_by(
+            'product__category')
+
     def get_item(pk):
         return Basket.objects.filter(pk=pk).first()
+
+    @property
+    def cost(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+        # products = Basket.objects.filter(user=self.user)
+        # total = sum([x.quantity for x in products])
+        # return total
+
+    @property
+    def total_cost(self):
+        _items = self.get_items_cached
+        return sum(list(map(lambda x: x.cost, _items)))
+
+        # products = Basket.objects.filter(user=self.user)
+        # cost = sum([x.cost for x in products])
+        # return cost
 
     # def save(self, *args, **kwargs):
     #     if self.pk:
@@ -43,19 +72,3 @@ class Basket(models.Model):
     #     self.product.quantity += self.quantity
     #     self.product.save()
     #     super(self.__class__, self).delete()
-
-    @property
-    def cost(self):
-        return self.product.price * self.quantity
-
-    @property
-    def total_quantity(self):
-        products = Basket.objects.filter(user=self.user)
-        total = sum([x.quantity for x in products])
-        return total
-
-    @property
-    def total_cost(self):
-        products = Basket.objects.filter(user=self.user)
-        cost = sum([x.cost for x in products])
-        return cost
