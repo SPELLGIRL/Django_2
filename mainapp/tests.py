253f736d04057114c232d4c50bdc845ffb1ae9ca
SPELLGIRL <chepushilka@yandex.ru>
2019-03-24 18:45:09 +0300

    5) Проведено тестирование работоспособности приложения mainapp.

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test.client import Client
+from mainapp.models import Product, CatalogMenu
+from django.core.management import call_command
+
+
+class TestMainappSmoke(TestCase):
+    def setUp(self):
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
+
+    def test_mainapp_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/contacts/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/featured/')
+        self.assertEqual(response.status_code, 200)
+
+        for category in CatalogMenu.objects.all().select_related('category'):
+            response = self.client.get(f'/products/{category.category.name}/')
+            self.assertEqual(response.status_code, 200)
+
+        for product in Product.objects.all():
+            response = self.client.get(f'/products/details/{product.pk}/')
+            self.assertEqual(response.status_code, 200)
+
+    def tearDown(self):
+        call_command('sqlsequencereset', 'mainapp', 'authapp', 'ordersapp',
+                     'basketapp')
